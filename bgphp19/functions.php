63b74046db8c0c0a009f32921dc87a4d6b84e5ba
Milko Kosturkov <mkosturkov@gmail.com>
2019-03-02 17:12:53 +0200

    Make organizer notice smaller on mobile

--- bgphp19/functions.php
+++ bgphp19/functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function load_styles() {
-    wp_enqueue_style('bgphp19-main', get_template_directory_uri() . '/style.css', [], '14');
+    wp_enqueue_style('bgphp19-main', get_template_directory_uri() . '/style.css', [], '15');
 }
 
 
