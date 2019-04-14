2513cd53ba1b52f9ed9e7835d987abd7f78ae6d4
Milko Kosturkov <mkosturkov@gmail.com>
2019-04-14 15:55:37 +0300

    WIP: Sponsor's page

---
+++ bgphp19/page-sponsors.php
@@ -0,0 +1,78 @@
+<?php include __DIR__ . '/header.php';?>
+    <div class="content-holder">
+        <article class="the-content restrained-width">
+            <h1>Become a Sponsor</h1>
+            <p>We are determined to create the most EPIC PHP CONFERENCE ever!</p>
+            <p>Get on board!</p>
+            <a href="https://www.bgphp.org/Bulgaria%20PHP%20Conference%202019%20Partnership%20Packs.pdf" class="call-for-sponsors button" target="_blank">
+                Partner with us
+            </a>
+        </article>
+    </div>
+    <div class="content-holder">
+        <article class="the-content restrained-width">
+            <h1>Our awesome partners</h1>
+            <ol>
+                <li>
+                    <h2>Bootstrap &amp; and Conference Founder</h2>
+                    <ul>
+                        <li>
+                            <a href="https://www.siteground.com/" title="SiteGround" target="_blank">
+                                <img
+                                    src="<?php echo get_template_directory_uri();?>/images/sponsor-logos/siteground.svg"
+                                    alt="SiteGround Logo"
+                                >
+                            </a>
+                            <div>
+                                SiteGround is a leading web hosting company with more than 12 years in the industry
+                                and over 500 000 hosted domains.
+                                Wе focus on top server performance and improved
+                                security with best in class all-SSD infrastructure,
+                                custom in-house developed solutions and latest speed technologies
+                                like NGINX, HHVM, PHP7 and HTTP/2
+                                that make websites 100x faster and better protected.
+                                We are famous for our unmatched customer service thanks to our fast,
+                                experienced and super friendly team providing amazing help 24/7.
+                                Encouraged by our very own PHP dev team,
+                                we are proud to help nurture our local PHP community by supporting
+                                the Bulgarian PHP user group and getting involved in Bulgaria PHP Conference
+                                from the very start.
+                                <a href="https://www.siteground.com/" title="SiteGround" target="_blank">
+                                    www.siteground.com
+                                </a>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <h2>Bootstrap &amp; and Conference Founder</h2>
+                    <ul>
+                        <li>
+                            <a href="https://www.isobar.com/bg/en/" title="isobar" target="_blank">
+                                <img
+                                    src="<?php echo get_template_directory_uri();?>/images/sponsor-logos/isobar.png"
+                                    alt="Isobar Logo"
+                                    id="isobar-logo"
+                                >
+                            </a>
+                            <div>
+                                Isobar Commerce Bulgaria, previously known as eCommera,
+                                has a proven track record for delivering, managing and innovating
+                                multi-channel commerce platforms for major international brand
+                                and retailers including Clarins, Walt Disney, Adidas, ASDA – WalMart, Shiseido
+                                and many more. Our world - class team has delivered sites for over 25 brands
+                                with more than 170 sites across 34 markets.
+
+                                Isobar Commerce partners with Salesforce Commerce Cloud,
+                                a leading e-commerce SaaS platform globally platform,
+                                globally to deliver outstanding value for retailers and brands.
+                                The team works to solve retail’s most pressing challenges –
+                                omni-channel operations, global expansion and customer experience.
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+            </ol>
+        </article>
+    </div>
+<?php include __DIR__ . '/footer.php';
