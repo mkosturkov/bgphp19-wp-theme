fca5170acb78d227642032031da6a1ccc3fa6d62
Milko Kosturkov <mkosturkov@gmail.com>
2019-04-17 21:35:40 +0300

    Style the sponsors' page

--- bgphp19/page-sponsors.php
+++ bgphp19/page-sponsors.php
@@ -1,6 +1,6 @@
 <?php include __DIR__ . '/header.php';?>
     <div class="content-holder">
-        <article class="the-content restrained-width">
+        <article class="the-content restrained-width call-for-sponsors-block">
             <h1>Become a Sponsor</h1>
             <p>We are determined to create the most EPIC PHP CONFERENCE ever!</p>
             <p>Get on board!</p>
@@ -10,33 +10,37 @@
         </article>
     </div>
     <div class="content-holder">
-        <article class="the-content restrained-width">
-            <h1>Our awesome partners</h1>
+        <article class="restrained-width sponsors-details">
             <ol>
-                <li>
-                    <h2>Bootstrap &amp; and Conference Founder</h2>
+                <li class="sponsors-details-group">
+                    <h2>Bootstrap &amp; Conference Founder</h2>
                     <ul>
-                        <li>
-                            <a href="https://www.siteground.com/" title="SiteGround" target="_blank">
+                        <li class="sponsors-details-single">
+                            <a class="sponsors-details-single-img-holder" href="https://www.siteground.com/" title="SiteGround" target="_blank">
                                 <img
                                     src="<?php echo get_template_directory_uri();?>/images/sponsor-logos/siteground.svg"
                                     alt="SiteGround Logo"
+                                    id="siteground-logo"
                                 >
                             </a>
-                            <div>
-                                SiteGround is a leading web hosting company with more than 12 years in the industry
-                                and over 500 000 hosted domains.
-                                Wе focus on top server performance and improved
-                                security with best in class all-SSD infrastructure,
-                                custom in-house developed solutions and latest speed technologies
-                                like NGINX, HHVM, PHP7 and HTTP/2
-                                that make websites 100x faster and better protected.
-                                We are famous for our unmatched customer service thanks to our fast,
-                                experienced and super friendly team providing amazing help 24/7.
-                                Encouraged by our very own PHP dev team,
-                                we are proud to help nurture our local PHP community by supporting
-                                the Bulgarian PHP user group and getting involved in Bulgaria PHP Conference
-                                from the very start.
+                            <div class="the-content an-article">
+                                <p>
+                                    SiteGround is a leading web hosting company with more than 12 years in the industry
+                                    and over 500 000 hosted domains.
+                                    Wе focus on top server performance and improved
+                                    security with best in class all-SSD infrastructure,
+                                    custom in-house developed solutions and latest speed technologies
+                                    like NGINX, HHVM, PHP7 and HTTP/2
+                                    that make websites 100x faster and better protected.
+                                </p>
+                                <p>
+                                    We are famous for our unmatched customer service thanks to our fast,
+                                    experienced and super friendly team providing amazing help 24/7.
+                                    Encouraged by our very own PHP dev team,
+                                    we are proud to help nurture our local PHP community by supporting
+                                    the Bulgarian PHP user group and getting involved in Bulgaria PHP Conference
+                                    from the very start.
+                                </p>
                                 <a href="https://www.siteground.com/" title="SiteGround" target="_blank">
                                     www.siteground.com
                                 </a>
@@ -44,30 +48,37 @@
                         </li>
                     </ul>
                 </li>
-                <li>
-                    <h2>Bootstrap &amp; and Conference Founder</h2>
+                <li class="sponsors-details-group">
+                    <h2>Gold</h2>
                     <ul>
-                        <li>
-                            <a href="https://www.isobar.com/bg/en/" title="isobar" target="_blank">
+                        <li class="sponsors-details-single">
+                            <a class="sponsors-details-single-img-holder" href="https://www.isobar.com/bg/en/" title="isobar" target="_blank">
                                 <img
                                     src="<?php echo get_template_directory_uri();?>/images/sponsor-logos/isobar.png"
                                     alt="Isobar Logo"
                                     id="isobar-logo"
                                 >
                             </a>
-                            <div>
-                                Isobar Commerce Bulgaria, previously known as eCommera,
-                                has a proven track record for delivering, managing and innovating
-                                multi-channel commerce platforms for major international brand
-                                and retailers including Clarins, Walt Disney, Adidas, ASDA – WalMart, Shiseido
-                                and many more. Our world - class team has delivered sites for over 25 brands
-                                with more than 170 sites across 34 markets.
+                            <div class="the-content an-article">
+                                <p>
+                                    Isobar Commerce Bulgaria, previously known as eCommera,
+                                    has a proven track record for delivering, managing and innovating
+                                    multi-channel commerce platforms for major international brand
+                                    and retailers including Clarins, Walt Disney, Adidas, ASDA – WalMart, Shiseido
+                                    and many more. Our world - class team has delivered sites for over 25 brands
+                                    with more than 170 sites across 34 markets.
+                                </p>
 
-                                Isobar Commerce partners with Salesforce Commerce Cloud,
-                                a leading e-commerce SaaS platform globally platform,
-                                globally to deliver outstanding value for retailers and brands.
-                                The team works to solve retail’s most pressing challenges –
-                                omni-channel operations, global expansion and customer experience.
+                                <p>
+                                    Isobar Commerce partners with Salesforce Commerce Cloud,
+                                    a leading e-commerce SaaS platform globally platform,
+                                    globally to deliver outstanding value for retailers and brands.
+                                    The team works to solve retail’s most pressing challenges –
+                                    omni-channel operations, global expansion and customer experience.
+                                </p>
+                                <a href="https://www.isobar.com/bg/en/" title="isobar" target="_blank">
+                                    www.isobar.com
+                                </a>
                             </div>
                         </li>
                     </ul>
